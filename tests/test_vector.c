9638626b510968cdd058a9bd39d4a40dc2b0a574
imaginarygarden <133775115+imaginarygarden@users.noreply.github.com>
2026-05-02 07:32:38 +0000

    0.1.1
    * Deleted inline unit testing library
    
    * Added Unity as submodule
    
    * Submodule initialization + additional tests
    
    * CI config updated to build tests
    
    * CHANGELOG.md updated

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -1,4 +1,4 @@
-#include <unity.h>
+#include "unity.h"
 #include "vectorlib.h"
 
 vector_t vec;
@@ -10,6 +10,8 @@ void setUp(void) {
 void tearDown(void) {
     vector_free(&vec);
 }
+
+/* ===================== BASIC TESTS ===================== */
 
 void test_init(void) {
     TEST_ASSERT_EQUAL(0, vector_size(&vec));
@@ -63,6 +65,129 @@ void test_out_of_bounds(void) {
     TEST_ASSERT_EQUAL(VECTOR_RES_ERR, vector_get(&vec, 0, &out));
 }
 
+/* ===================== EXTENDED TESTS ===================== */
+
+void test_push_multiple_order(void) {
+    for (int i = 0; i < 10; i++) {
+        vector_push_back(&vec, &i);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        int out;
+        vector_get(&vec, i, &out);
+        TEST_ASSERT_EQUAL(i, out);
+    }
+}
+
+void test_pop_until_empty(void) {
+    int val = 1;
+    vector_push_back(&vec, &val);
+
+    int out;
+    TEST_ASSERT_EQUAL(VECTOR_RES_OK, vector_pop_back(&vec, &out));
+    TEST_ASSERT_EQUAL(1, out);
+
+    TEST_ASSERT_EQUAL(VECTOR_RES_ERR, vector_pop_back(&vec, &out));
+}
+
+void test_capacity_growth(void) {
+    size_t initial = vector_capacity(&vec);
+
+    for (size_t i = 0; i < initial + 1; i++) {
+        vector_push_back(&vec, &i);
+    }
+
+    TEST_ASSERT_GREATER_THAN(initial, vector_capacity(&vec));
+}
+
+void test_set_out_of_bounds(void) {
+    int val = 10;
+    TEST_ASSERT_EQUAL(VECTOR_RES_ERR, vector_set(&vec, 0, &val));
+}
+
+void test_get_last_element(void) {
+    for (int i = 0; i < 5; i++) {
+        vector_push_back(&vec, &i);
+    }
+
+    int out;
+    vector_get(&vec, 4, &out);
+
+    TEST_ASSERT_EQUAL(4, out);
+}
+
+void test_large_data_integrity(void) {
+    for (int i = 0; i < 1000; i++) {
+        vector_push_back(&vec, &i);
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        int out;
+        vector_get(&vec, i, &out);
+        TEST_ASSERT_EQUAL(i, out);
+    }
+}
+
+void test_size_consistency(void) {
+    int val = 0;
+
+    for (int i = 0; i < 20; i++) {
+        vector_push_back(&vec, &val);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        vector_pop_back(&vec, &val);
+    }
+
+    TEST_ASSERT_EQUAL(10, vector_size(&vec));
+}
+
+void test_init_zero_size(void) {
+    vector_t v;
+    TEST_ASSERT_EQUAL(VECTOR_RES_ERR, vector_init(&v, 0));
+}
+
+void test_free_resets_vector(void) {
+    int val = 1;
+    vector_push_back(&vec, &val);
+
+    vector_free(&vec);
+
+    TEST_ASSERT_EQUAL(0, vec.size);
+    TEST_ASSERT_EQUAL(0, vec.capacity);
+    TEST_ASSERT_NULL(vec.data);
+
+    // re-init so tearDown doesn't double free broken state
+    vector_init(&vec, sizeof(int));
+}
+
+/* ===================== GENERIC TYPE TEST ===================== */
+
+typedef struct {
+    int a;
+    int b;
+} pair_t;
+
+void test_struct_storage(void) {
+    vector_free(&vec);
+    vector_init(&vec, sizeof(pair_t));
+
+    pair_t p = {1, 2};
+    vector_push_back(&vec, &p);
+
+    pair_t out;
+    vector_get(&vec, 0, &out);
+
+    TEST_ASSERT_EQUAL(1, out.a);
+    TEST_ASSERT_EQUAL(2, out.b);
+
+    // restore int vector for other tests safety
+    vector_free(&vec);
+    vector_init(&vec, sizeof(int));
+}
+
+/* ===================== TEST RUNNER ===================== */
+
 int main(void) {
     UNITY_BEGIN();
 
@@ -72,6 +197,17 @@ int main(void) {
     RUN_TEST(test_resize_growth);
     RUN_TEST(test_set);
     RUN_TEST(test_out_of_bounds);
+
+    RUN_TEST(test_push_multiple_order);
+    RUN_TEST(test_pop_until_empty);
+    RUN_TEST(test_capacity_growth);
+    RUN_TEST(test_set_out_of_bounds);
+    RUN_TEST(test_get_last_element);
+    RUN_TEST(test_large_data_integrity);
+    RUN_TEST(test_size_consistency);
+    RUN_TEST(test_init_zero_size);
+    RUN_TEST(test_free_resets_vector);
+    RUN_TEST(test_struct_storage);
 
     return UNITY_END();
 }
